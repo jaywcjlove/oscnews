369cd73f502c154789738b21f8380d2d6b0bad4b
jaywcjlove <398188662@qq.com>
2018-01-29 00:25:00 +0800

    添加设置按钮

---
+++ src/component/modal/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './index.less';
+
+export default class Modal extends Component {
+  render() {
+    return (
+      <div>
+        <div className={styles.mask}>
+
+        </div>
+
+      </div>
+    )
+  }
+}
