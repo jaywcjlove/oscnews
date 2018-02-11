a4cd98174324612d58dce11144fb70c5f198e1ad
jaywcjlove <398188662@qq.com>
2018-02-11 20:23:16 +0800

    修复在Chrome扩展中解决X-Frame-Options 拒绝问题

--- chrome-main/background.js
+++ chrome-main/background.js
@@ -1,6 +1,22 @@
 
-function createTabs() {
+chrome.browserAction.onClicked.addListener(function () {
   chrome.tabs.create({ url: 'chrome://newtab' });
-}
+});
 
-chrome.browserAction.onClicked.addListener(createTabs);
+chrome.webRequest.onHeadersReceived.addListener(
+  function (info) {
+    var headers = info.responseHeaders;
+    for (var i = headers.length - 1; i >= 0; --i) {
+      var header = headers[i].name.toLowerCase();
+      if (header == 'x-frame-options' || header == 'frame-options') {
+        headers.splice(i, 1); // Remove header
+      }
+    }
+    return { responseHeaders: headers };
+  },
+  {
+    urls: ['*://*/*'], // Pattern to match all http(s) pages
+    types: ['sub_frame']
+  },
+  ['blocking', 'responseHeaders']
+);
