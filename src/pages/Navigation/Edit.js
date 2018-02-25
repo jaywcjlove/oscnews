2e19b7c30f47e656d935e22558c959fa01201c6d
jaywcjlove <398188662@qq.com>
2018-02-25 17:43:53 +0800

    更新搜索和导航数据, 添加开发文档地址选择

--- src/pages/Navigation/Edit.js
+++ src/pages/Navigation/Edit.js
@@ -7,7 +7,7 @@ import website from '../../source/website.json';
 import addIcon from '../../assets/add-icon.png';
 
 const blankData = {
-  label: '添加网址标题',
+  label: '',
   value: '',
   icon: addIcon,
 };
@@ -35,11 +35,16 @@ export default class Edit extends Component {
         label: item.title,
         value: item.website,
         icon: item.logo || websiteIcon,
+        urls: item.urls || '',
+        _website: item.website,
         type: 'doc',
       };
     });
-    websiteSource.push(documentSource);
-    this.setState({ websiteSource });
+    const doc = websiteSource.filter(item => item.value === 'document');
+    if (doc && doc.length === 0) {
+      websiteSource.push(documentSource);
+      this.setState({ websiteSource });
+    }
   }
   setListData(type) {
     let list = [];
@@ -101,15 +106,38 @@ export default class Edit extends Component {
     edit.label = value;
     this.setState({ edit });
   }
+  handleClickEditBtn(url) {
+    const { edit } = this.state;
+    edit.value = url;
+    this.setState({ edit });
+  }
+  renderEditItemURL(edit) {
+    const { urls, label } = edit;
+    const items = [];
+    for (const a in urls) {
+      if (Object.prototype.hasOwnProperty.call(urls, a)) {
+        let title = a;
+        if (a === 'git') title = '开源仓库';
+        if (a === 'cn') title = '中文文档';
+        items.push(<span key={a} onClick={this.handleClickEditBtn.bind(this, urls[a])}>{title}</span>);
+      }
+    }
+    if (items.length > 1) items.push(<span onClick={this.handleClickEditBtn.bind(this, edit._website)} key={label}>官网</span>);
+    return items;
+  }
   render() {
     const { active, edit, websiteSource } = this.state;
     return (
       <div className={styles.navEdit} ref={node => this.warpper = node}>
         <div className={classNames(styles.edit, { doc: active === 'document' })}>
           <img alt={edit.label} onError={e => e.target.src = websiteIcon} src={edit.icon} />
-          <span className={styles.title}><input type="text" onChange={this.onChangeTitleEdit.bind(this)} className={styles.titleInput} value={edit.label} />{edit.label}</span>
+          <span className={styles.title}>
+            <input type="text" placeholder="添加网址标题" onChange={this.onChangeTitleEdit.bind(this)} className={styles.titleInput} value={edit.label} />
+            {edit.label}
+          </span>
           <input className={styles.url} onChange={this.onChangeEdit.bind(this)} value={edit.value} type="text" placeholder="输入网址" />
-          <button className={styles.save} onClick={this.onClickAdd.bind(this, edit)}>确定</button>
+          <button className={styles.save} onClick={this.onClickAdd.bind(this, edit)}>添加</button>
+          {active === 'document' && edit.urls && <div className={styles.otherUrl}>{this.renderEditItemURL(edit)}</div>}
         </div>
         <div className={styles.editTabList}>
           <div className={styles.category}>
